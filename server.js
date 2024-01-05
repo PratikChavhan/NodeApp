0279d1f4d691eebafbf54eadfdb9b21659e18606
Pratik Chavhan <77968699+PratikChavhan@users.noreply.github.com>
2024-01-05 17:30:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = reqiure("express");
+const express = require("express");
 const path = require("path");
 const app = express();
 const staticfolder = express.static(path.join(__dirname, "public"));
